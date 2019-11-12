27b0c129f6439f86565abef377ca3fa47477f062
Banayaki <artemmuha@gmail.com>
2019-11-12 10:31:35 +0400

    update lab_5 (SAS)

--- DataBase/lab_5.sql
+++ DataBase/lab_5.sql
@@ -1,36 +1,37 @@
 -- Task 1
 select *
 from EMPLOYEES emp
-where emp.NUM in
+where emp.NUM not in
       (
-          select DISTINCT emp.num
-          from EMPLOYEES emp
-                   left join JOB_HISTORY jh on emp.NUM = JH.NUM
-          group by emp.NUM, emp.JOB, jh.JOB
-          having count(emp.NUM) = 1
-             and emp.JOB = NVL(jh.JOB, emp.JOB)
+--           Подзапрос. ID рабочих, которые меняли работу.
+          select distinct jh_1.NUM
+          from JOB_HISTORY jh_1
+                   join JOB_HISTORY jh_2 on jh_1.NUM = jh_2.NUM
+          where jh_1.JOB != jh_2.JOB
+          group by jh_1.NUM
       );
 
 -- Task 2
 select emp.FNAME
 from EMPLOYEES emp
 where emp.NUM in (
-    select DISTINCT emp.NUM
-    from EMPLOYEES emp
-             left join JOB_HISTORY jh on emp.NUM = JH.NUM
-    where MONTHS_BETWEEN(jh.END_DATE, SYSDATE) <= 12
-    group by emp.NUM, emp.JOB, jh.JOB
-    having count(emp.NUM) > 1
+--     Подзапрос. Таблица с работниками которые меняли работу за последний год
+    select distinct jh_1.NUM
+    from JOB_HISTORY jh_1
+             left join JOB_HISTORY jh_2 on jh_1.NUM = jh_2.NUM
+    where MONTHS_BETWEEN(SYSDATE, NVL(jh_1.END_DATE, SYSDATE)) <= 12
+      and MONTHS_BETWEEN(SYSDATE, NVL(jh_2.END_DATE, SYSDATE)) <= 12
+      and jh_1.JOB != jh_2.JOB
 );
 
 -- Task 3
 select NUM_DEPARTMENT
-from (select jh.NUM_DEPARTMENT, count(jh.NUM_DEPARTMENT)
-      from EMPLOYEES emp
-               left join JOB_HISTORY jh on emp.NUM = JH.NUM
-      where emp.job != NVL(jh.job, emp.job)
-      group by jh.NUM_DEPARTMENT
-      order by count(jh.NUM_DEPARTMENT) DESC)
+from (select jh_1.NUM_DEPARTMENT, count(jh_1.NUM_DEPARTMENT)
+      from JOB_HISTORY jh_1
+               left join JOB_HISTORY jh_2 on jh_1.NUM = jh_2.NUM
+      where jh_1.JOB != jh_2.JOB
+      group by jh_1.NUM_DEPARTMENT
+      order by count(jh_1.NUM_DEPARTMENT) DESC)
 where ROWNUM = 1;
 
 -- Task 4
